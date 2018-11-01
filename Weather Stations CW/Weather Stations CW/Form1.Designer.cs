28f9af13505247cb4336bda4377b1d2e731bc673
Josh Franklin <josh-franklin-@hotmail.co.uk>
2018-11-01 21:43:13 +0000

    creating the files

---
+++ Weather Stations CW/Weather Stations CW/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Weather_Stations_CW
+{
+    partial class WeatherInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WeatherInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 552);
+            this.Name = "WeatherInformation";
+            this.Text = "Weather Information";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
